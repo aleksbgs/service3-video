19dd65d4b1c2e73f9fc8feabafeea9e4a5ea848b
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2022-01-23 19:07:03 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var build = "develop"
+
+func main() {
+	log.Println("starting service", build)
+	defer log.Printf("service ended")
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	<-shutdown
+
+	log.Println("stopping service")
+
+}
